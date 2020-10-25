694a296a1a86912f1b1afcec1e02d0b9c0996fcd
paul-romeo <countmedream@yahoo.com>
2020-10-24 17:31:26 -0700

    added Numbers.kt

---
+++ src/Numbers.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // Basic math operators: + - / *
+    val a = 12
+    val b = 25
+    println(a + b)
+    println(a - b)
+    println(b / a)
+    println(b * a)
+
+    // modulo or remainder
+    println(b % a)
+
+}
